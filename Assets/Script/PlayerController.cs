521baca2d71bfabb0b4f063fbc6b636762c0bbcf
Agung Gunawan <agg.nst772@gmail.com>
2022-12-12 21:40:13 +0700

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
 
     public Animator animatorMove, animatorBody;
 
+    public GameObject textDamage;
     private void Awake()
     {
         instance = this;
